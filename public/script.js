f8ed9016c3c8ed374a508d790574a2e5f29598df
Josh Skousen <skousenjosh@gmail.com>
2019-11-25 19:51:35 +0000

    Midterm 2 -- Unfinished

---
+++ public/script.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [],
+    },
+    methods: {
+        async getItems() {
+            try {
+                let response = await axios.get("/api/items");
+                this.items = response.data;
+                return true;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    created() {
+        this.getItems();
+    },
+});
